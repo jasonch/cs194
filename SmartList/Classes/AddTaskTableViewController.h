181a2f7964e2ffc7e1a159464a702bc4df2173fa
Justine DiPrete <Justine@dn0a203161.sunet>
2011-04-20 18:10:11 -0700

    clean project

---
+++ SmartList/Classes/AddTaskTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddTaskTableViewController.h
+//  SmartList
+//
+//  Created by Justine DiPrete on 4/18/11.
+//  Copyright 2011 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AddTaskTableViewController : UITableViewController {
+	NSManagedObjectContext *context;
+	UISlider *slider;
+}
+
+-initInManagedObjectContext:(NSManagedObjectContext*)aContext;
+
+@end
---
+++ SmartList/Classes/WhatNowViewController.m
@@ -0,0 +1,85 @@
+//
+//  WhatNowViewController.m
+//  SmartList
+//
+//  Created by Justine DiPrete on 4/18/11.
+//  Copyright 2011 Stanford. All rights reserved.
+//
+
+#import "WhatNowViewController.h"
+
+
+@implementation WhatNowViewController
+
+-(void) setup
+{
+	self.title = @"What Now?";	
+	UITabBarItem *item = [[UITabBarItem alloc] initWithTitle: @"What Now?" image:[UIImage imageNamed: @"78-stopwatch.png"] tag:0];
+	self.tabBarItem = item;
+	[item release];
+}
+
+-initInManagedObjectContext:(NSManagedObjectContext*)aContext
+{
+	context = aContext;
+	[self setup];
+	return self;
+}
+
+
+-(IBAction)startPressed:(UIButton*)sender
+{
+	
+}
+
+
+-(IBAction)donePressed:(UIButton*)sender
+{
+	
+}
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	[self setup];
+}
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
