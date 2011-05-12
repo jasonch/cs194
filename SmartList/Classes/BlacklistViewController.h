561783c2b5f1665695372b42bebf887abe3d7c4c
Anna Shtengelova <illusence@gmail.com>
2011-05-12 11:59:03 -0700

    Blacklist files

---
+++ SmartList/Classes/BlacklistViewController.h
@@ -0,0 +1,21 @@
+//
+//  BlacklistViewController.h
+//  SmartList
+//
+//  Created by Anna Shtengelova on 5/11/11.
+//  Copyright 2011 Stanford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ViewTaskViewController.h"
+#import "CoreDataTableViewController.h"
+#import "Task.h"
+
+@interface BlacklistViewController : UITableViewController {
+	NSManagedObjectContext *context;
+	NSMutableArray *blacklist;
+}
+
+-initInManagedObjectContext:(NSManagedObjectContext*)aContext withBlacklist:(NSMutableArray*)aBlacklist;
+
+@end
